75894c857ff7334bb50251e2e4f727009c96d475
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-04-11 13:38:27 +0800

    push to github

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="col-md-3">
+    <ul class="list-group">
+        <li class="list-group-item">
+            <a href="{{ url('/home') }}">
+                Dashboard
+            </a>
+        </li>
+        <li class="list-group-item">
+                <a href="{{ url('/admin/role') }}">
+                    Role
+                </a>
+            </li>
+        <li class="list-group-item">
+            <a href="{{ url('/admin/permission') }}">
+                Permission
+            </a>
+        </li>
+        <li class="list-group-item">
+            <a href="{{ url('/admin/user') }}">
+                Users
+            </a>
+        </li>
+    </ul>
+</div>
